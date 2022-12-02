e3c053fcf102621ea3791a16162b81479c1089f4
Maksim Barsukov <maksim.barsukov@adastra.one>
2022-12-02 17:24:01 +0100

    Added new page with conference showing

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,26 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
-    {
-        return new Response(
-            <<<EOF
-<html>
-    <body style="
-        background-image: url('/images/under-construction.png'); 
-        background-position: center;
-        background-repeat: no-repeat;
-    ">
-    </body>
-</html>
-EOF
+    public function index(
+        ConferenceRepository $repository
+    ): Response {
+        return  $this->render('conference/index.html.twig', [
+                'conferences' => $repository->findAll(),
+            ]
+        );
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(
+        Request $request,
+        Conference $conference,
+        CommentRepository $commentRepository
+    ) {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+        return  $this->render('conference/show.html.twig',
+            [
+                'conference' => $conference,
+                'comments' => $paginator,
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
+            ]
         );
     }
 }
